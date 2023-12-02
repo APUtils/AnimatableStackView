558b3a254d4e2d822b6bec5bdad2ecead480f369
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-12-02 21:58:45 +0600

    Revert "- Manual layout"
    This reverts commit cf1be4887379b25a208ebd19a67de09b07194173.

--- AnimatableView/Classes/_Extensions/UIView+Utils.swift
+++ AnimatableView/Classes/_Extensions/UIView+Utils.swift
@@ -25,6 +25,64 @@ extension UIView {
             self.frame = frame
         }
     }
+    
+    /// Preserve view's size using autoresizing mask during layout.
+    func layoutSubviewsOnly() {
+        guard !subviews.isEmpty else { return }
+        
+        let activeOuterConstraints = getOuterConstraints().filter { $0.isActive }
+        
+        // Disable outer constraints to prevent broken constraints during layout
+        if !activeOuterConstraints.isEmpty {
+            NSLayoutConstraint.deactivate(activeOuterConstraints)
+        }
+        
+        if translatesAutoresizingMaskIntoConstraints {
+            layoutIfNeeded()
+            
+        } else {
+            // Fix view position and size during layout using autoresize mask
+            translatesAutoresizingMaskIntoConstraints = true
+            layoutIfNeeded()
+            translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        // Restore outer constraints
+        if !activeOuterConstraints.isEmpty {
+            NSLayoutConstraint.activate(activeOuterConstraints)
+        }
+    }
+    
+    /// Get all constraints to `self` from outer view hierarchy.
+    func getOuterConstraints() -> [NSLayoutConstraint] {
+        superviews
+            .flatMap { $0.constraints(to: self) }
+            .sorted { $0.priority > $1.priority }
+    }
+    
+    /// Returns contraints to a `view` that source view owns.
+    /// - Parameter view: A view to use for a check.
+    func constraints(to view: UIView) -> [NSLayoutConstraint] {
+        constraints.filter { $0.firstItem === view || $0.secondItem === view }
+    }
+    
+#if compiler(>=5)
+    /// All view superviews to the top most
+    var superviews: DropFirstSequence<UnfoldSequence<UIView, (UIView?, Bool)>> {
+        return sequence(first: self, next: { $0.superview }).dropFirst(1)
+    }
+#else
+    /// All view superviews to the top most
+    var superviews: AnySequence<UIView> {
+        return sequence(first: self, next: { $0.superview }).dropFirst(1)
+    }
+#endif
+    
+    var allSubviews: [UIView] {
+        var allSubviews = self.subviews
+        allSubviews.forEach { allSubviews.append(contentsOf: $0.allSubviews) }
+        return allSubviews
+    }
 }
 
 
